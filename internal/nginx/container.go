97ff57cdc4e862e4fbe021663cf02efa4eec1ff9
dannyvidal <danny@dannyvidal.com>
2022-08-06 17:25:26 -0400

    nginx creations

---
+++ internal/nginx/container.go
@@ -0,0 +1,46 @@
+package nginx
+
+import (
+	_ "embed"
+	"fmt"
+
+	"github.com/dannyvidal/blah/internal/containers"
+	"github.com/dannyvidal/blah/internal/utils"
+	"github.com/docker/docker/api/types/mount"
+)
+
+var (
+	//go:embed nginx.conf
+	nginxConf []byte
+
+	DefaultImgTag = "nginx:latest"
+)
+
+func InitialSetup(projectName string, fn containers.CallbackFn) (containers.ContainerCreator, error) {
+	nginxPath := utils.WriteFileAbs(nginxConf, "nginx.conf")
+	container := containers.Container{
+		Name:     utils.PrefixProjectName(projectName, "nginx"),
+		Image:    DefaultImgTag,
+		Hostname: fmt.Sprintf("com.%s.nginx", projectName),
+		Mounts: []containers.ContainerMount{
+			{
+				Type:   mount.TypeBind,
+				Source: nginxPath,
+				Tagret: "/etc/nginx/nginx.conf",
+			},
+		},
+		ExposedPorts: []containers.ContainerExposedPort{
+			{
+				Port: "80",
+			},
+		},
+		PortBindings: []containers.ContainerPortBinding{
+			{
+				Port:     "80",
+				HostPort: "8080",
+				HostIP:   "0.0.0.0",
+			},
+		},
+	}
+	return containers.NewCreateContainerPayload(&container, fn), nil
+}
